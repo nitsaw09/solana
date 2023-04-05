52fdc5398e0a562e96fddfdf26c159f64b2f6ea5
nitesh ghuge <niteshghuge09@gmail.com>
2023-04-05 07:51:42 +0000

    transaction-and-feee

--- index.js
+++ index.js
@@ -4,7 +4,10 @@ const {
     PublicKey,
     clusterApiUrl,
     Keypair,
-    LAMPORTS_PER_SOL
+    LAMPORTS_PER_SOL,
+    Transaction,
+    SystemProgram,
+    sendAndConfirmTransaction
 } = require("@solana/web3.js");
 
 // Create a new keypair
@@ -55,11 +58,65 @@ const airDropSol = async () => {
     }
 };
 
+const transferSol = async() => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+
+    // Get Keypair from Secret Key
+    var from = Keypair.fromSecretKey(privateKey);
+
+    // Generate another Keypair (account we'll be sending to)
+    const to = Keypair.generate();
+
+    // Aidrop 2 SOL to Sender wallet
+    console.log("Airdopping some SOL to Sender wallet!");
+    const fromAirDropSignature = await connection.requestAirdrop(
+        new PublicKey(from.publicKey),
+        2 * LAMPORTS_PER_SOL
+    );
+
+    // Latest blockhash (unique identifer of the block) of the cluster
+    let latestBlockHash = await connection.getLatestBlockhash();
+
+    // Confirm transaction using the last valid block height (refers to its time)
+    // to check for transaction expiration
+    await connection.confirmTransaction({
+        blockhash: latestBlockHash.blockhash,
+        lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+        signature: fromAirDropSignature
+    });
+
+    console.log("Airdrop completed for the Sender account");
+
+    const senderBalance = await connection.getBalance(
+        new PublicKey(newPair.publicKey)
+    );
+
+    const amountToSend = senderBalance / 2;
+
+    // Send money from "from" wallet and into "to" wallet
+    var transaction = new Transaction().add(
+        SystemProgram.transfer({
+            fromPubkey: from.publicKey,
+            toPubkey: to.publicKey,
+            lamports: amountToSend
+        })
+    );
+
+    // Sign transaction
+    var signature = await sendAndConfirmTransaction(
+        connection,
+        transaction,
+        [from]
+    );
+    console.log('Signature is', signature);
+}
+
 // Show the wallet balance before and after airdropping SOL
 const mainFunction = async () => {
     await getWalletBalance();
     await airDropSol();
     await getWalletBalance();
+    await transferSol()
 }
 
 mainFunction();
